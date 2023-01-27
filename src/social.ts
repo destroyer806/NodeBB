83f35cee41cf7f8784a42330dcf5a9691ec2fa4f
Victor Huang <victorhuangwq@gmail.com>
2023-01-27 13:59:14 -0500

    using chat-gpt for p1

--- src/social.ts
+++ src/social.ts
@@ -1,65 +1,50 @@
-// This is one of the two example TypeScript files included with the NodeBB repository
-// It is meant to serve as an example to assist you with your HW1 translation
+import * as _ from 'lodash';
+import * as plugins from './plugins';
+import * as db from './database';
 
-import _ from 'lodash';
-import plugins from './plugins';
-import db from './database';
+export const social = {
+    postSharing: null,
 
-import { Network } from './types';
+    async getPostSharing(): Promise<Array<{ id: string; name: string; class: string; activated: boolean }>> {
+        if (social.postSharing) {
+            return _.cloneDeep(social.postSharing);
+        }
 
-let postSharing: Network[] | null = null;
+        let networks = [
+            {
+                id: 'facebook',
+                name: 'Facebook',
+                class: 'fa-facebook',
+                activated: null,
+            },
+            {
+                id: 'twitter',
+                name: 'Twitter',
+                class: 'fa-twitter',
+                activated: null,
+            },
+        ];
+        networks = await plugins.hooks.fire('filter:social.posts', networks);
+        const activated = await db.getSetMembers('social:posts.activated');
+        networks.forEach((network) => {
+            network.activated = activated.includes(network.id);
+        });
 
-export async function getPostSharing(): Promise<Network[]> {
-    if (postSharing) {
-        return _.cloneDeep(postSharing);
-    }
+        social.postSharing = networks;
+        return _.cloneDeep(networks);
+    },
 
-    let networks: Network[] = [
-        {
-            id: 'facebook',
-            name: 'Facebook',
-            class: 'fa-facebook',
-            activated: null,
-        },
-        {
-            id: 'twitter',
-            name: 'Twitter',
-            class: 'fa-twitter',
-            activated: null,
-        },
-    ];
+    async getActivePostSharing(): Promise<Array<{ id: string; name: string; class: string; activated: boolean }>> {
+        const networks = await social.getPostSharing();
+        return networks.filter(network => network && network.activated);
+    },
 
-    networks = await plugins.hooks.fire('filter:social.posts', networks) as Network[];
-
-    // The next line calls a function in a module that has not been updated to TS yet
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    const activated: string[] = await db.getSetMembers('social:posts.activated') as string[];
-
-    networks.forEach((network) => {
-        network.activated = activated.includes(network.id);
-    });
-
-    postSharing = networks;
-    return _.cloneDeep(networks);
-}
-
-export async function getActivePostSharing(): Promise<Network[]> {
-    const networks: Network[] = await getPostSharing();
-    return networks.filter(network => network && network.activated);
-}
-
-export async function setActivePostSharingNetworks(networkIDs: string[]): Promise<void> {
-    postSharing = null;
-
-    // The next line calls a function in a module that has not been updated to TS yet
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    await db.delete('social:posts.activated');
-
-    if (!networkIDs.length) {
-        return;
-    }
-
-    // The next line calls a function in a module that has not been updated to TS yet
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    await db.setAdd('social:posts.activated', networkIDs);
-}
+    async setActivePostSharingNetworks(networkIDs: string[]): Promise<void> {
+        social.postSharing = null;
+        await db.delete('social:posts.activated');
+        if (!networkIDs.length) {
+            return;
+        }
+        await db.setAdd('social:posts.activated', networkIDs);
+    },
+};
